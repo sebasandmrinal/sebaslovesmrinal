a9e8a42fe06ddbb2f9b4abd29886a5cbf96825c1
sebasandmrinal <workwithdheeraj7@gmail.com>
2025-10-19 17:33:17 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,13 @@
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
+import path from "path"
 
+export default defineConfig({
+  base: '/sebaslovesmrinal/', // ← Change this line
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
