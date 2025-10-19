2c58d56927bcd162f22bb5a0dd76b429571d2254
sebasandmrinal <workwithdheeraj7@gmail.com>
2025-10-19 17:47:47 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,21 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Questions from './pages/Questions'
+import SpecialMessage from './pages/SpecialMessage'
 
+function App() {
+  return (
+    <Router basename="/romantic-website">
+      <div className="min-h-screen bg-background">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/questions" element={<Questions />} />
+          <Route path="/special-message" element={<SpecialMessage />} />
+        </Routes>
+      </div>
+    </Router>
+  )
+}
+
+export default App
